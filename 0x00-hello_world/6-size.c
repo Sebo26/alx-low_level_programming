a10ebc411a157e0e518e4c09d23ef4644c17aad6
Sebongile <sebomaduna26@gmail.com>
2023-09-07 12:41:56 -0700

    No 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a %s: %d byte(s)", "char", 1);
+printf("Size of a %s: %d byte(s)", "int", 4);
+printf("Size of a %s: %d byte(s)", "long int", 4);
+printf("Size of a %s: %d byte(s)", "long long int", 8);
+printf("Size of a %s: %d byte(s)", "float", 4);
+return (0);
+}
