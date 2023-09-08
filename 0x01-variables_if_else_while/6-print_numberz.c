33eaf9747900685cda17dd686983e5124c6e336a
Sebongile <sebomaduna26@gmail.com>
2023-09-08 16:38:13 -0700

    No 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,4 +3,19 @@
 /**
  * main -Entry point
  *
- * Description: 
+ * Description: The code prints the single digits of base 10 from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+
+return (0);
+}
