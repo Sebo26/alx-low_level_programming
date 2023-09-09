ace8f9d0717ee93ad7139022ac7b767fc2ce5bbe
Sebongile <sebomaduna26@gmail.com>
2023-09-09 04:58:40 -0700

    No 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Code prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+	putchar('0' + n);
+	putchar(',');
+	putchar(' ');
+}
+putchar('$');
+
+return (0);
+}
