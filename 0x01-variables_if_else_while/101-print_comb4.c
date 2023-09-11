99a8a77f18f551bbf951a0bd38d2eb5ba597308e
Sebongile <sebomaduna26@gmail.com>
2023-09-11 09:30:21 -0700

    No 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,13 +17,15 @@ for (n = 0; n < 9; n++)
 for (m = n + 1; m <= 9; m++)
 {
 for (l = 2; l<=9; l++)
+{
 	putchar('0' + n);
 	putchar('0' + m);
 	putchar('0' + l);
-if (n != 8 || m != 9 || l != 9)
+if (n != 7 || m != 8 || l != 9)
 {
 	putchar(',');
 	putchar(' ');
+}
 }
 }
 }
