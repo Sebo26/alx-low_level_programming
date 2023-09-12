6f27d5197ab96f8d3832bad302d09a2d4d276770
Sebongile <sebomaduna26@gmail.com>
2023-09-12 10:12:24 -0700

    No 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include <main.h>
+
+/**
+ * function_putchar - Prints "_putchar" followed by a new line.
+ *
+ * Description: This function prints the string "_putchar" followed by
+ * a new line character to the standard output.
+ */
+void function_putchar(void)
+{
+	printf("_putchar\n");
+}
 
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line.
+ * Description: This is the main function that calls function_putchar
+ * to print "_putchar" followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("_putchar\n");
+	function_putchar();
 
-return (0);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,24 @@
 #ifndef MAIN-HEADER_file
 #define MAIN-HEADER_file
-int
+void function_putchar()
+{
+	printf("_putchar\n");
+}
+#endif
+
+#ifndef MAIN-HEADER_file
+#define MAIN-HEADER_file
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 #endif
