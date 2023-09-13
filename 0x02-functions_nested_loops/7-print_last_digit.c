38bf12c23262304bdaa7e00a9a2a862b40cfa572
Sebongile <sebomaduna26@gmail.com>
2023-09-13 16:27:39 -0700

    No 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,14 @@ int print_last_digit(int n)
 int m;
 if (n >= 0)
 {
-m = (n % 10) * 11;
+m = n % 10;
 _putchar('0' + m);
 }
 else
 {
-m = (n % 10) * -1 * 11;
+m = (n % 100) * -1;
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
 }
 
 return (m);
