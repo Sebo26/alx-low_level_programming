1da95422089bd14cce7771bed28feeb5265f00db
Sebongile <sebomaduna26@gmail.com>
2023-09-14 02:31:31 -0700

    No 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14, 10 times followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+int m;
+int n;
+for (m = 0; m <= 14; m++)
+{
+	for (n = 0; n < 10; n++)
+	{
+		_putchar (m);
+		_putchar ('\n');
+	}
+}
+}
