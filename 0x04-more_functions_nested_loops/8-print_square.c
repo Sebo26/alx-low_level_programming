5a9cd5fd75d59c495cda5427a8d4058d423242cb
Sebongile <sebomaduna26@gmail.com>
2023-09-15 09:14:21 -0700

    No 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_square - The function prints a followed by a new line.
+ * print_square - The function prints a '#' followed by a new line.
  *
  * @size: it is the size of the square
  * Return: size
  */
 void print_square(int size)
 {
-int m;
-int n;
+int m = 1;
+int n = 1;
 if (size > 0)
 {
-	while (m == size)
+	while (m <= size)
 	{
-		while (n == size)
+		while (n <= size)
 		{
 			_putchar ('#');
 			n++;
