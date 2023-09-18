129b5e0a5fdf79664550449cffcc3ce3b6d55614
Sebongile <sebomaduna26@gmail.com>
2023-09-18 16:52:45 -0700

    No 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - The function prints a string followed by a new line to the
+ * standard output.
+ * @str: The string that will be on the standard output
+ */
+void _puts(char *str)
+{
+str = puts();
+}
