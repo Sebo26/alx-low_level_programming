97323a85173c20a50c9ce2f2497314cb71dcee51
Sebongile <sebomaduna26@gmail.com>
2023-09-19 08:39:21 -0700

    No 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - The function prints string in reverse followed by new line.
+ * @s: The string that will be printed in reverse.
+ */
+void print_rev(char *s)
+{
+int i;
+char temp;
+int l = strlen(s);
+int midpoint = l/2;
+
+for (i = 0; i < midpoint; i++)
+{
+	temp = s[i];
+	s[i] = s[l - i - 1];
+	s[l - i - 1] = temp;
+}
+}
