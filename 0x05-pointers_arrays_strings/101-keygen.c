6b3aabe9f2936dc4cfa37cd9cd9f0e781d46e1b6
Sebongile <sebomaduna26@gmail.com>
2023-09-19 15:42:48 -0700

    No 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,28 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+
+/**
+ * password - The function generatea a random password
+ *
+ * Description: To generate a password
+ */
+int password(void)
+{
+srand(time(0));
+int p = (rand () / 100);
+printf("%d", p);
+}
 
 /**
  * main - Entry point
  *
  * Description: The program generates valid password.
- * return: p (password)
+ * return: 0 (Success)
  */
 int main(void)
 {
-srand(time(0));
-int p = (rand () / 100);
-return (p);
+password();
+
+return (0);
 }
