465165d5240edc649228f333c195181e5f0aa886
Sebongile <sebomaduna26@gmail.com>
2023-09-20 06:17:34 -0700

    No 0

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN-H
-#define MAIN_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
