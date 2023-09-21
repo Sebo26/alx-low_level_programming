6b96b7b4d52265b3a6eeb4931745bb7c1774b8d4
Sebongile <sebomaduna26@gmail.com>
2023-09-21 10:10:12 -0700

    No 5

---
+++ 0x06-pointers_arrays_strings/5-strin_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - The function changes all lowercase letters of a string to uppercase.
+ *
+ * @*: The string that will have lowercase letters changed to uppercase.
+ * Return: * (The string that has been changed to uppercase.
+ */
+char *string_toupper(char *)
+{       
+int length;
+length = strlen(s);
+int i;
+for (i = 0; i < length; i++)
+{
+	*[i] = toupper(s]i]);
+}
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - The function changes all lowercase letters of a
+ * string to uppercase.
+ * @*: The string that will change lowercase letters to uppercase.
+ * Return: * (String that will have lowercase changed into uppercase.
+ */
+char *string_toupper(char *)
+{
+int length;
+int length = strlen(*);
+int i;
+for (i = 0; i < length; i++)
+{
+	s[i] = toupper(*[i]);
+}
+return (*);
+}
