57c9afe691be2c1e42c1a49741f3c560e4aaa57c
Sebongile <sebomaduna26@gmail.com>
2023-09-21 02:41:19 -0700

    No 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,16 +7,10 @@
  * @dest: The string to which src will be added.
  * @src: The string that will have n bytes of it added to dest.
  * @n: number of bytes of src that will be added to dest.
+ * Return: dest (pointer to the resulting string)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int src_length;
-src_length = strlen(src);
-
-for (i = 0 ; i < n && src[n] != '\0' ; n++)
-{
-	dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-}
-	return (dest);
+strncat(dest, src, n);
+return (dest);
 }
