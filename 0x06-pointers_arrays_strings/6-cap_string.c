5b838ef9619dacee1027336f215f66e75035b2b1
Sebongile <sebomaduna26@gmail.com>
2023-09-21 10:32:53 -0700

    No 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *cap_string - The function capitalizes all words of a string.
+ * @s: The string that will have all words capitalized.
+ * Return: s ( the string capitalised)
+ */
+char *cap_string(char *)
+
