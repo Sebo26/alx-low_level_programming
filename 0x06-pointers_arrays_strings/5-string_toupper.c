1ea0075c6f64b5a8dec22b7b567f3bb843d10059
Sebongile <sebomaduna26@gmail.com>
2023-09-22 13:50:25 -0700

    No 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,14 +4,14 @@
 /**
  * *string_toupper - The function changes all lowercase letters of a
  * string to uppercase.
- * @*: The string that will change lowercase letters to uppercase.
+ * @s: The string that will change lowercase letters to uppercase.
  * Return: s (String that will have lowercase changed into uppercase.
  */
 char *string_toupper(char *s)
 {
+int i;
 int length;
 length = strlen(s);
-int i;
 for (i = 0; i < length; i++)
 {
 	if (islower(s[i]))
