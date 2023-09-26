c195d213c89fb69f01b2021647d71c3021d9d29b
Sebongile <sebomaduna26@gmail.com>
2023-09-26 05:28:33 -0700

    No 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_chessboard - The function prints a chessboard.
+ * @a: The rows
+ * @[8]: The 8 colomns that will be printed
+ * Return: 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+
