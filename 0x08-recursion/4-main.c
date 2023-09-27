731f2113f9e8b3355927db769fcc401b5226002e
Sebongile <sebomaduna26@gmail.com>
2023-09-27 09:59:43 -0700

    NO 4

--- 0x08-recursion/4-main.c
+++ 0x08-recursion/4-main.c
@@ -1,16 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _pow_recursion - The function returns the value of x raised
- * to the power of y
- * @x: The base of the exponent
- * @y: The power to which x will be raised
- * Return: -1 (when y is lower than 0) or value of x to power of y
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int _pow_recursion(int x, int y)
+int main(void)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	return (
+    int r;
+
+    r = _pow_recursion(1, 10);
+    printf("%d\n", r);
+    r = _pow_recursion(1024, 0);
+    printf("%d\n", r);
+    r = _pow_recursion(2, 16);
+    printf("%d\n", r);
+    r = _pow_recursion(5, 2);
+    printf("%d\n", r);
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
+    printf("%d\n", r);
+    return (0);
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - The function returns the value of x raised
+ * to the power of y
+ * @x: The base of the exponent
+ * @y: The power to which x will be raised
+ * Return: -1 (when y is lower than 0) or value of x to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+                return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
