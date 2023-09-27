e7c27f97e97b65893fdbac90742d13283c4d237b
Sebongile <sebomaduna26@gmail.com>
2023-09-27 12:20:51 -0700

    No 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,3 +6,10 @@
  * Return: -1 (if n doesn't have a natural square root)
  */
 int _sqrt_recursion(int n)
+{
+	if (_sqrt_recursion(n) < 0)
+	{
+	return (-1);
+	}
+	return (n * _sqrt_recursion(n));
+}
