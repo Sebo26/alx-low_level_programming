9f6684eee6668368c5487cfe042eb1b73930f848
Sebongile <sebomaduna26@gmail.com>
2023-09-28 14:08:18 -0700

    No 1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,20 @@
  * @n: The number that will be squared.
  * Return: -1 (if n doesn't have a natural square root)
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n)
+if (n < 0)
+{
+	return (-1);
+	return (help_sqrt_recursion(n, 0));
+}
+
+/**
+ * _sqrt_recursion - The function squares numbers 1 and upwards until = n.
+ * @i: number that will be squared to get n (iterator)
+ * @n: the number that will have its square root determined.
+ * Return: Square root.
+ */
+int help_sqrt_recursion(int n, int i)
 {
 	if (i * i == n)
 		return (i);
@@ -14,5 +27,5 @@ int _sqrt_recursion(int n, int i)
 	if (i * i > n)
 		return (-1);
 
-	return _sqrt_recursion(n, i + 1);
+	return help_sqrt_recursion(n, i + 1);
 }
