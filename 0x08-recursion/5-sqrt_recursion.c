840c550d006ae5d1442bd936d7c2d88cc6c18506
Sebongile <sebomaduna26@gmail.com>
2023-09-28 17:35:32 -0700

    No 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,8 +26,7 @@ int help_sqrt_recursion(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-        return (-1);
-
-        return (_sqrt_recursion(-1));
+	if (n < 0)
+	return (-1);
+	return (_sqrt_recursion(-1));
 }
