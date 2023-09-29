e780241fc63d22ecd9ed7e8d229b83dfce747124
Sebongile <sebomaduna26@gmail.com>
2023-09-28 18:14:02 -0700

    No 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * wildcmp - The function compares two strings.
+ * @s1: The first string
+ * @s2: The second string
+ * Return: 1 (identical strings), otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == *s2)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
