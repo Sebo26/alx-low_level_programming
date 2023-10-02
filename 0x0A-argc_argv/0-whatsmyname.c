782425d3e05f1c08de2bf064dea485a0878c5079
Sebongile <sebomaduna26@gmail.com>
2023-10-02 01:47:46 -0700

    No 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * @argv: The string that will be printed.
  * Return: 0 (Success)
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
