5a00589f73283fe32ae938d440cef7d6ec065b66
Sebongile <sebomaduna26@gmail.com>
2023-10-03 04:02:41 -0700

    No 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * *str_concat - The function concatenates two strings.
+ * @s1: The first string.
+ * @s2: The string that will be joined to s1.
+ * Return: string (newly allocated space in memory containing s1, s2 '\0')
+ * or NULL (failure or empty string)
+ */
+char *str_concat(char *s1, char *s2)
+{
+
