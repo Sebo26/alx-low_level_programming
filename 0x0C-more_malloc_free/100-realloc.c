51d356f69d6311012687047c834a8127e6609241
Sebongile <sebomaduna26@gmail.com>
2023-10-05 14:43:22 -0700

    No 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,3 +11,28 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
+if (new_size > old_size)
+{
+	ptr = "";
+}
+
+ptr = malloc(new_size);
+if (ptr == NULL)
+{
+	ptr = malloc(new_size);
+}
+
+if (new_size == 0)
+{
+	free(ptr);
+	return (NULL);
+}
+
+if (new_size == old_size)
+{
+	return (ptr);
+}
+
+free(ptr);
+return (ptr);
+}
