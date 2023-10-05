16e896bff159f2eb97551879fdd7dd7e17841b81
Sebongile <sebomaduna26@gmail.com>
2023-10-05 04:28:09 -0700

    No 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,5 +13,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
 int length;
-length = 
-str = 
+int j;
+length = strlen(strncpy(s1, s2, n));
+str = malloc((length + 1) * sizeof(char));
+
+if (str == NULL)
+{
+	return (NULL);
+}
+
+for (j = 0; s1[j] != '\0'; j++)
+{
+	str[j] = s1[j];
+}
+
+for (n = 0; s2[n] != '\0'; n++)
+{
+	str[j + n] = s2[n];
+}
+str[length] = '\0';
+return (str);
+}
