b610d75b16fdc2bc43dc0405282e87572998b9fa
Sebongile <sebomaduna26@gmail.com>
2023-10-05 12:50:51 -0700

    No 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,14 +11,23 @@
 int *array_range(int min, int max)
 {
 int i;
+int *arr;
+int size;
+
 if (min > max)
 {
 	return (NULL);
 }
-
+size = max - min + 1;
+arr = malloc(size * sizeof(int));
 if (arr == NULL)
 {
 	return (NULL);
 }
 
-for (
+for (i = min; i <= max; i++)
+{
+	arr[i] = min + i;
+}
+return (arr);
+}
