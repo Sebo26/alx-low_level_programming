483f6a0703ff98184679ce1232a5be4f9d3ee84e
Sebongile <sebomaduna26@gmail.com>
2023-10-05 15:07:38 -0700

    No 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: The program multiplies two positive numbers.
+ * @mul: The product of two positive numbers.
+ * @num1: The first number that will be multiplied.
+ * @num2: The second number that will be multiplied.
+ */
+void main(int argc, char argv)
+{
+int mul;
+unsigned int num1;
+unsigned int num2;
+
