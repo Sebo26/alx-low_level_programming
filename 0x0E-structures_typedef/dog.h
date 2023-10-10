3fe32cb7fb69d2e7f43447be04ef79c7f0b6fe7a
Sebongile <sebomaduna26@gmail.com>
2023-10-09 17:02:11 -0700

    No 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - has elements about a dog
+ * @name: Dog name.
+ * @age: Age of dog.
+ * @owner: The person who owns it.
+ *
+ * The struct will store information about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
