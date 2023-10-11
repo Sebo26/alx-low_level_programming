3c421fba020e76298a6d0de9379b08b2cb4df560
Sebongile <sebomaduna26@gmail.com>
2023-10-11 07:43:59 -0700

    No 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,4 +9,13 @@
  * @action: Pointer to the array function.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
-
+{
+if (array == NULL || action == NULL)
+{
+}
+while (size > 0)
+{
+	action(*array);
+	array++;
+}
+}
