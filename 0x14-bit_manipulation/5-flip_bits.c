d59f03c8319d6a6edacb103b2ae90c993c18530f
Sebongile <sebomaduna26@gmail.com>
2023-10-27 10:07:13 -0700

    No 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,3 +4,21 @@
 /**
  * flip_bits - The function returns the number of bits you would need to flip
  * to get from one number to another.
+ * @n: The first number of bits.
+ * @m: The number of bits that will be compared to the first.
+ * Return: The number of changes that need to be made for digits to be the same
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int result;
+unsigned int diff;
+diff = 0;
+result = n ^ m;
+
+while (result > 0)
+{
+	diff += result & 1;
+	result >>= 1;
+}
+return (diff);
+}
