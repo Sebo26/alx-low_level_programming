c214b95f2a311526fbb36706a7fff512fba84b6a
root <sebomaduna26@gmail.com>
2023-12-07 05:05:45 -0800

    No 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-const dlistint_t *curr = h;
 size_t i;
 i = 0;
-while (curr != NULL)
+while (h)
 {
-	printf("%d\n", curr->n);
-	curr = curr->next;
+	printf("%d\n", h->n);
+	h = h->next;
 	i++;
 }
 return(i)
