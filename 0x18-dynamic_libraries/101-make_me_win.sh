b87c819fc92549c8a7d3917941188d4d22edf872
root <sebomaduna26@gmail.com>
2023-12-15 06:24:00 -0800

    No 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P
+wget -P .. https://raw.githubusercontent.com/Sebo26/alx-low_level_programming/master/0x18-dynamic_libraries/libnumbers.so
+export LD_PRELOAD="$PWD/../libnumbers.so"
