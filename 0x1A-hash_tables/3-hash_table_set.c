107a5494d1630446c60da89f7e1d15912b5dd8cf
root <sebomaduna26@gmail.com>
2023-12-22 14:46:12 -0800

    No 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,22 +11,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *new_node;
 if (ht == NULL || key == NULL || value == NULL)
-{       
+{
 	return (0);
 }
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 {
-        free(new_node);
-        return 0;
+	free(new_node);
+	return (0);
 }
-new_node->value = strdup(value);
 
+new_node->key = strdup(key);
+if (new_node->key == NULL)
+{
+	free(new_node);
+	return (0);
+}
+
+new_node->value = strdup(value);
 if (new_node->value == NULL)
 {
 	free(new_node->key);
 	free(new_node);
-	return 0;
+	return (0);
 }
 return (1);
 }
