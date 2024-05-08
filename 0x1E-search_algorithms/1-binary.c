bca8ac7aa3f7e84810094b2effe20470905b3310
root <sebomaduna26@gmail.com>
2024-05-08 02:37:11 -0700

    No 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,50 +12,33 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-        int left = 0;
-        int right = size - 1;
-        int middle;
-        int m;
-        size_t i;
+	int left = 0;
+	int right = size - 1;
+	int middle;
 
-        printf("Searching in array: ");
-        if (array == NULL)
-        {
-                printf("Value checked array[%u] = [%s]\n", 0, "NULL");
-                return (-1);
-        }
-        for (i = 0; i < size; ++i)
-        {
-        printf(" %d", array[i]);
-        if (i < size - 1)
-                printf(",");
-        }
-        printf("\n");
+	if (array == NULL)
+	{
+		printf("Value checked array[%u] = [%s]\n", 0, "NULL");
+		return (-1);
+	}
 
-        while (left <= right)
-        {
-                middle = left + (right - left) / 2;
-                if (array[middle] == value)
-                {
-                        return (middle);
-                }
+	while (left <= right)
+	{
+		middle = left + (right - left) / 2;
 
-                printf("Searching in array: ");
-                for (m = left; m <= right; ++m)
-                {
-                        printf(" %d", array[m]);
-                        if (m < right)
-                                printf(",");
-                }
-                printf("\n");
-                if (array[middle] < value)
-                {
-                        left = middle + 1;
-                }
-                else
-                {
-                        right = middle - 1;
-                }
-        }
-        return (-1);
+		if (array[middle] == value)
+		{
+			return (middle);
+		}
+
+		if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else
+		{
+			right = middle - 1;
+		}
+	}
+	return (-1);
 }
