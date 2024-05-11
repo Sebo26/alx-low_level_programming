320f061b8b17390e74a40b19d6fd8ffbbc6cc94d
root <sebomaduna26@gmail.com>
2024-05-11 09:57:42 -0700

    No 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,7 +3,7 @@
 
 /**
  * jump_search - searches for a value in an array of integers.
- * 
+ *
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array.
  * @value: the value searched for.
