62066b4aa061dba30e44ec54d46491811440573b
root <sebomaduna26@gmail.com>
2024-05-11 09:38:49 -0700

    No 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in an array of integers.
+ * 
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array.
+ * @value: the value searched for.
+ * Return: -1 (if value is not present or if it is NULL) or index of value
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int step = sqrt(size);
+	int prev = 0;
+	int curr = step;
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (curr < (int)size && array[curr] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		prev = curr;
+		curr += step;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", prev, curr);
+
+	for (i = prev; i < (int)size && i <= curr; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
+
